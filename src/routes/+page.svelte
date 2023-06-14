e9b925e294cdc31635bcd800ed4bf742b49e4d74
agonzd <agonzd@uc.cl>
2023-06-14 02:52:28 -0400

    :star:: Added highscore persistence for the game

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
 <script>
   import { onMount } from "svelte";
-  import axios from "axios";
+  import { scores } from '../stores.js';
+  import Game from "./Game.svelte";
+  import Scoreboard from "./Scoreboard.svelte";
 
   let gameType = "population";  // New variable for tracking game type
-  let loadingAtStart  = true;
+  let loadingAtStart = true;
   let gameStarted = false;  
 
-  import Game from "./Game.svelte";
   function startGame() {
     gameStarted = true;
   }
+
+  // Persist store taken from
+  // https://steveolensky.medium.com/persist-your-svelte-store-between-page-refreshes-in-a-few-lines-of-code-8dc36fc926a6
+  let savestore = false;
+  $: if (savestore && $scores) {
+    window.sessionStorage.setItem("store", JSON.stringify($scores))
+  }
+  onMount(async () => {
+    let ses = window.sessionStorage.getItem("store")
+      if (ses) {
+        $scores = JSON.parse(ses)
+      }
+    savestore = true
+  })
 </script>
 
 
@@ -24,7 +39,8 @@
     <option value="gdp">GDP</option>
     <option value="unemployment">Unemployment</option>
   </select>
-  <button on:click={startGame} >Start Game</button>  
+  <button on:click={startGame}>Start Game</button>
+  <Scoreboard/>
   <p>Falta:</p>
   <ul>
     <li>Mejor CSS</li>
